3734e90d7814cc4b7eb916779621e8a3a6ee1475
dylanjian0 <dylanjian0@gmail.com>
2024-06-24 15:51:22 -0400

    final

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,26 +6,32 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
+        <div className="split left">
           <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
+          <img className="profile_image" src={profile_pic} alt="Profile Pic"/>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Dylan Jian</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              👋 Nice to meet you, I'm Dylan! <br></br>
+              📚 I'm a student at The Ohio State University studying computer science
+              and engineering and quantitative economics. <br></br>
+              🏫 On campus, I'm involved in the Chinese American Student 
+              Association. <br></br>
+              😃 In my free time, I like to code, play the piano, soccer, do competitive 
+              Rubik's Cubing, and play Go (board game). <br></br>
+              💻 I'm currently participating in IBM's Accelerate program on the 
+              software development track, a SWE intern 
+              at Y Stem and Chess, and building a full-stack project on
+              the side. <br></br>
+              🛠️ My tech stack includes: Java, Django, React, Tailwind CSS, SQL, 
+              MATLAB, and CAD technologies such as OnShape. 
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
